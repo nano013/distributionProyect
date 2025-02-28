83e944e617736fd6d2292d7a0618ba8f566fc3ee
AXEL EMANUEL FLEITA <afleita@csjn.gov.ar>
2025-02-28 10:51:37 -0300

    first commit

---
+++ src/main/java/ar/distribution/App.java
@@ -0,0 +1,5 @@
+package ar.distribution;
+
+public class App {
+
+}
---
+++ src/main/java/ar/distribution/design/jFrame.java
@@ -0,0 +1,45 @@
+ package ar.distribution.design;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+
+public class jFrame {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					jFrame window = new jFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public jFrame() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 419, 291);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
